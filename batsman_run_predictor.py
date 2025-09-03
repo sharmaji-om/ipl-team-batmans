d0e0d89e6adab8b986d3d42836dd956fc8b88525
Om Sharma <123316255+sharmaji-om@users.noreply.github.com>
2025-09-03 11:16:21 +0530

    Update batsman_run_predictor.py

--- batsman_run_predictor.py
+++ batsman_run_predictor.py
@@ -184,7 +184,7 @@ def run_batsman_run_app():
     numeric_cols = ["Matches Played", "Avg Runs per Match", "Strike Rate", 
                     "Batting Position", "Previous Match Runs", "Form (Last 5 Innings Avg.)"]
 
-    dataset = pd.read_csv(r"C:\Users\om718\OneDrive\Desktop\New folder (4)\ipl_batsmen_current_players_no_team.csv")
+    dataset =  pd.read_csv("https://raw.githubusercontent.com/sharmaji-om/ipl-team-batmans/main/data/ipl_batsmen_current_players_no_team.csv") 
     categorical_cols = ["Player", "Opponent Team", "Venue"]
     label_encoders = {}
     for col in categorical_cols:
@@ -392,4 +392,5 @@ def run_batsman_run_app():
         except Exception as e:
             st.error(f"❌ Error in prediction: {e}")
             st.info("Please check your input values and try again.")
+
 
