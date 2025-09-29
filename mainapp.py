3292c04df410e2b319252054579380f1f8873880
Om Sharma <123316255+sharmaji-om@users.noreply.github.com>
2025-09-29 18:48:00 +0530

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,14 +1,19 @@
 import streamlit as st
-st.set_option("client.showErrorDetails", False)
 from team_win_predictor import run_team_win_app
 from batsman_run_predictor import run_batsman_run_app
 
 st.sidebar.title("IPL Prediction Navigation")
-app_mode = st.sidebar.selectbox("Choose an app", ["Team Win Predictor", "Batsman Run Predictor"])
+app_mode = st.sidebar.selectbox(
+    "Choose an app", 
+    ["Team Win Predictor", "Batsman Run Predictor"]
+)
 
-if app_mode == "Team Win Predictor":
-    run_team_win_app()
-elif app_mode == "Batsman Run Predictor":
-    run_batsman_run_app()
-
-
+try:
+    if app_mode == "Team Win Predictor":
+        run_team_win_app()
+    elif app_mode == "Batsman Run Predictor":
+        run_batsman_run_app()
+except UnboundLocalError:
+   
+    st.empty()
+   
